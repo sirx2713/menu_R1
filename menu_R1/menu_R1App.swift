11e065b57e9334b00ce163eba72cc182072fb8d3
Mabende <belindamabende@gmail.com>
2024-05-09 12:03:19 +0200

    Initial Commit

---
+++ menu_R1/menu_R1App.swift
@@ -0,0 +1,17 @@
+//
+//  menu_R1App.swift
+//  menu_R1
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/05/09.
+//
+
+import SwiftUI
+
+@main
+struct menu_R1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
